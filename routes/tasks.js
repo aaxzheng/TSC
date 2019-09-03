4a1ed36d8dc0d50dec7618f791f2a0e47b31c5ff
aaxzheng <aaronzheng3@gmail.com>
2019-09-03 17:22:11 -0400

    added delete route and update route

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 const validateTaskInput = require('../validation/valid-task');
 router.get("/test", (req, res) => res.json({ msg: "This is the tasks route" }));
 
+// get all tasks
 router.get("/all" ,(res,req) => {
     Task.find({}, (err, tasks) => {
         let alltasks = {};
@@ -17,7 +18,7 @@ router.get("/all" ,(res,req) => {
         req.send(alltasks);
     })
 });
-
+//get user's tasks
 router.get('/user/:user_id', (req, res) => {
     Task.find({ user: req.params.user_id })
         .then(tasks => res.json(tasks))
@@ -26,9 +27,9 @@ router.get('/user/:user_id', (req, res) => {
             )
         );
 });
-
+//get specific task
 router.get('/:id', (req, res) => {
-    Tweet.findById(req.params.id)
+    Task.findById(req.params.id)
         .then(task => res.json(task))
         .catch(err =>
             res.status(404).json({ notaskfound: 'No tasks found with that ID' })
@@ -36,6 +37,7 @@ router.get('/:id', (req, res) => {
 
 });
 
+//add new tasks
 router.post('/',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
@@ -64,5 +66,53 @@ router.post('/',
     }
 );
 
+//remove tasks
+router.delete('/:id', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Task.findById(req.params.id)
+            .then((task) => {
+                Task.deleteOne(task);
+                return res.json({msg: "successful deletion"});
+            })
+            .catch(err =>
+                res.status(404).json({ notaskfound: 'No tasks found with that ID' })
+            );    
+    }
+); 
+
+
+//update tasks
+router.patch('/:id', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Task.findById(req.params.id)
+            .then((task) => {
+                const { errors, isValid } = validateTaskInput(req.body);
+                debugger;
+                if (!isValid) {
+                    return res.status(400).json(errors);
+                }
+                if (req.body.pub == 'true') {
+                    req.body.pub = true;
+                } else if (req.body.pub == 'false') {
+                    req.body.pub = false;
+                }
+                const newTask = {
+                    name: req.body.name,
+                    user: req.user.id,
+                    startTime: req.body.startTime,
+                    endTime: req.body.endTime,
+                    description: req.body.description,
+                    location: req.body.location,
+                    pub: req.body.pub,
+
+                };
+
+                task.updateOne(newTask).then(task => res.json(task));
+            })
+            .catch(err =>
+                res.status(404).json({ notaskfound: 'No tasks found with that ID' })
+            );    
+    }
+); 
 
 module.exports = router;
